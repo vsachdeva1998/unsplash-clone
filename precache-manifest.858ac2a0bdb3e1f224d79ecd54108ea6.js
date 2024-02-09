58d3abe6b352bebae00fd9605a7db7502b80daa6
Vinay <vsahdeva1998@gmail.com>
2024-02-09 15:28:56 +0530

    Updates

---
+++ precache-manifest.858ac2a0bdb3e1f224d79ecd54108ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5a529ebdc344ce1757334482453b36",
+    "url": "/vsachdeva1998/unsplash-clone/index.html"
+  },
+  {
+    "revision": "c4b704b1125bb04872c0",
+    "url": "/vsachdeva1998/unsplash-clone/static/css/main.6a1a0334.chunk.css"
+  },
+  {
+    "revision": "4f9b3421446f4e8856df",
+    "url": "/vsachdeva1998/unsplash-clone/static/js/2.230f2e3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vsachdeva1998/unsplash-clone/static/js/2.230f2e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b704b1125bb04872c0",
+    "url": "/vsachdeva1998/unsplash-clone/static/js/main.a3ca1a63.chunk.js"
+  },
+  {
+    "revision": "e4557c7e91c9507aa144",
+    "url": "/vsachdeva1998/unsplash-clone/static/js/runtime-main.296d8e83.js"
+  }
+]);
